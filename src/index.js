5b3b2386de09f43de7749f064c2a10f7164b356d
Rogshas <warehter@gmail.com>
2021-01-26 13:55:30 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    
+        let x = str.length + 1;
+        while(str = str.replace(/\<\>|\[\]|\{\}|\(\)|\|\||12|34|56|77|88/,'')){
+            if(str.length == x && x!=0)return false;
+            if(x==0)return true;
+            x=str.length;
+        }
+        return true;
+    
+    }
+
+
